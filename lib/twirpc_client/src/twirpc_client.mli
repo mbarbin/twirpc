7756a79e68608ae647b31745782b491ec3aa1c8f
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-04-15 15:19:23 +0200

    Wip client side

---
+++ lib/twirpc_client/src/twirpc_client.mli
@@ -0,0 +1,17 @@
+module Connection : sig
+  type t
+end
+
+val with_connection
+  :  host:string
+  -> port:int
+  -> f:(Connection.t -> 'a Or_error.t)
+  -> 'a Or_error.t
+
+(** call a given RPC [encoding] defaults to [JSON]. *)
+val call
+  :  ?encoding:[ `JSON | `BINARY ]
+  -> ('request, 'response) Twirpc_spec.t
+  -> connection:Connection.t
+  -> 'request
+  -> 'response Or_error.t
