a5c6d81de0605574965adad12494213244a6b671
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-04-23 18:54:42 +0200

    Relax signature constraint

--- lib/twirpc_client/src/twirpc_client.mli
+++ lib/twirpc_client/src/twirpc_client.mli
@@ -5,7 +5,7 @@ end
 (** Unix sockets are not supported by the underlying deps used in this project,
     so we've removed that functionality, and require a [port] here. The server
     is assumed to be running on the localhost. *)
-val with_connection : port:int -> f:(Connection.t -> 'a Or_error.t) -> 'a Or_error.t
+val with_connection : port:int -> f:(Connection.t -> 'a) -> 'a
 
 (** call a given RPC [encoding] defaults to [JSON]. *)
 val call
